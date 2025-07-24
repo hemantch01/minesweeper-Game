db72ad4dcc8df047451abd3e413b6696aed4f188
hemantch01 <hemantchaudhary905@gmail.com>
2025-07-24 05:05:11 +0000

    migrated to typescript

--- src/internals.ts
+++ src/internals.ts
@@ -4,11 +4,18 @@ export const states = {
     MINE :'mine',
     NUMBER:'number',
     MARKED:'marked'
+} as const;
+type Tile= {
+    element: HTMLDivElement;
+    i: number;
+    j: number;
+    mine: boolean;
+    status: any;
 }
 // make a board with element having custom key-value (dataset: DOMstringMap)
 // then add it to the board 
 // then define some getter and setter to change status of mine
-export function makeBoard (size,numberOfMines){
+export function makeBoard (size:number,numberOfMines:number){
     let arr = [];
     const minePositions = getMinePositions(size,numberOfMines);
     for(let i=0;i<size;i++){
@@ -32,7 +39,7 @@ export function makeBoard (size,numberOfMines){
     return arr;
 }
 
-function isvalid(pos,minesPos){
+function isvalid(pos:{x:number,y:number},minesPos:{x:number,y:number}[]){
     let flag = true;
     for(let i=0;i<minesPos.length;i++){
         if(pos.x===minesPos[i].x&&pos.y===minesPos[i].y){
@@ -42,13 +49,13 @@ function isvalid(pos,minesPos){
     return flag;
 }
 
-function getRandomEle(boardSize){
+function getRandomEle(boardSize:number){
     return Math.floor(Math.random()*boardSize);
 }
-function getMinePositions(boardSize ,numberOfMines){
+function getMinePositions(boardSize:number ,numberOfMines:number){
     const minesPos = [];
     while(minesPos.length<numberOfMines){
-        const pos = {
+        const pos :{x:number,y:number} = {
             x: getRandomEle(boardSize),
             y: getRandomEle(boardSize)
         }
@@ -59,7 +66,7 @@ function getMinePositions(boardSize ,numberOfMines){
     return minesPos;
 }
 
-export function mark(tile){
+export function mark(tile:Tile){
     if(tile.status!==states.HIDDEN&&tile.status!==states.MARKED)return;
     else if(tile.status==states.MARKED){
         tile.status= states.HIDDEN;
@@ -69,17 +76,19 @@ export function mark(tile){
     }
 }
 
-export function setMineCounter(board,NUMBER_OF_MINES){// update the number of mines according to the board.
+export function setMineCounter(board:Tile[][],NUMBER_OF_MINES:number){// update the number of mines according to the board.
     let ctr = 0;
     board.forEach(row => {
      row.forEach(tile=>{
         if(tile.status===states.MARKED)ctr++;
      })   
     });
-    document.getElementById("minesLeft").textContent = NUMBER_OF_MINES-ctr;
+    const leftCtr = String(NUMBER_OF_MINES-ctr);
+    const ele = document.getElementById("minesLeft");
+    if(ele) ele.textContent = leftCtr;
 }
 
-export function revealTile(board,tile){
+export function revealTile(board:Tile[][],tile:Tile){
     // if tile is a mine brust all
     if(tile.status!==states.HIDDEN)return;
     else {
@@ -91,7 +100,8 @@ export function revealTile(board,tile){
             tile.status = states.NUMBER;   
            const adjTiles = findAdjTiles(tile,board);
            const nearMines = adjTiles.filter(x=>x.mine);
-          if(nearMines.length>0)tile.element.textContent=nearMines.length;
+           const minesString = (nearMines.length).toString();
+          if(nearMines.length>0)tile.element.textContent=minesString;
           else{
             tile.element.textContent ='';
                adjTiles.forEach(adjTile => revealTile(board, adjTile));// logic for recursive calls 
@@ -100,7 +110,7 @@ export function revealTile(board,tile){
         }
     }
 }
-    function findAdjTiles(tile,board){
+    function findAdjTiles(tile:Tile,board:Tile[][]){
         const adjtiles = [];
         for(let i=-1;i<=1;i++){
             for(let j=-1;j<=1;j++){
@@ -114,7 +124,7 @@ export function revealTile(board,tile){
         return adjtiles;
     }
 
-function findNearMines(tile,board){/// used to find mines near the tiles---- but of no use
+function findNearMines(tile:Tile,board:Tile[][]){/// used to find mines near the tiles---- but of no use
     let nearMines = 0;
     for(let i=-1;i<=1;i++){
         for(let j=-1;j<=1;j++){
@@ -130,15 +140,15 @@ function findNearMines(tile,board){/// used to find mines near the tiles---- but
     return nearMines;
 }
 
-function isValidTile(size,x,y){
+function isValidTile(size:number,x:number,y:number){
     return x>=0&&y>=0&&x<size&&y<size;
 }
 
-export function checkWin(board){
+export function checkWin(board:Tile[][]){
     return board.every(row=>row.every(tile => (tile.status===states.NUMBER)||
     ((tile.status===states.HIDDEN||tile.status==states.MARKED)&&tile.mine===true))); //// ya to tile mine ho and unreveld ho ya tile number ho
 }
-export function checkLose(board){
+export function checkLose(board:Tile[][]){
 return board.some(row =>
     row.some(tile => tile.status === states.MINE)
 );
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { makeBoard,mark,revealTile,setMineCounter,checkLose,checkWin, states,  } from "./internals.js";
 
 const BOARD_SIZE = 10;
-export const NUMBER_OF_MINES = 1;
+export const NUMBER_OF_MINES = 10;
 const board = makeBoard(BOARD_SIZE,NUMBER_OF_MINES);
 const page = document.querySelector(".board");
 const message = document.querySelector(".subtext");
 board.forEach(row =>{
     row.forEach(tile=>{
-        page.append(tile.element);
+       if(page) page.append(tile.element);
         tile.element.addEventListener('click',()=>{    //adding the click functionality 
             // main revelaing function 
             revealTile(board,tile);
@@ -23,17 +23,18 @@ board.forEach(row =>{
         )
     })
 })
-document.getElementById("minesLeft").textContent = NUMBER_OF_MINES;
-page.style.setProperty('--size',BOARD_SIZE);
+const minesleftElement = document.getElementById("minesLeft")
+if(minesleftElement)  minesleftElement.textContent = NUMBER_OF_MINES.toString();
+if (page instanceof HTMLElement) page.style.setProperty('--size', BOARD_SIZE.toString());
 
 function checkWinOrLose(){
     const win =  checkWin(board);
     const lose = checkLose(board);
     if(win){
-        message.textContent="YOU WIN!"
+        if(message)message.textContent="YOU WIN!"
     }
     else if(lose){
-        message.textContent="YOU LOSE!"
+        if(message)message.textContent="YOU LOSE!"
         board.forEach(row=>{
             row.forEach(tile=>{
                 if(tile.status==states.MARKED)mark(tile);
@@ -45,10 +46,10 @@ function checkWinOrLose(){
     }
     
     if(win||lose){
-        page.addEventListener('click',(e)=>{
+        if(page)page.addEventListener('click',(e)=>{
             e.stopImmediatePropagation();
         },{capture:true});
-        page.addEventListener('click',(e)=>{
+        if(page)page.addEventListener('click',(e)=>{
             e.stopImmediatePropagation();
         },{capture:true})
     }
